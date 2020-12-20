3fa7a79a92cd95381e99be46efa1c73d9488e1ad
ismael <ismael@murabba.com>
2020-12-20 13:30:34 +0200

    Initial Commit

---
+++ SocketTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocketTask
+//
+//  Created by Ismael AlShabrawy on 20/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
